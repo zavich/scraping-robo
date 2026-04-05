e34a586bde0e4c4dde33ae00bd6d48e3bd70f372
rafaaelnicacio <rafaelnicacio154@gmail.com>
2026-04-05 20:05:02 -0300

    fix: remover log de resposta do servidor ao baixar PDF

--- src/modules/pje/services/fetch-documents-url.service.ts
+++ src/modules/pje/services/fetch-documents-url.service.ts
@@ -52,7 +52,6 @@ export class FetchDocumentoService {
         // responseType: 'arraybuffer',
         withCredentials: true,
       });
-      console.log('Resposta do servidor ao baixar PDF:', response.data);
 
       if (!response.data || response.data.length === 0) {
         throw new Error('Resposta vazia ao baixar PDF');
